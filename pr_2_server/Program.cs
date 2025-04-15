554182d877f892bfb7bac9cc1b835d3f5591bafa
Kirill <kirilkanote@gmail.com>
2025-04-15 16:15:35 +0300

    Добавьте файлы проекта.

---
+++ pr_2_server/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using pr_2_server.Models;
+
+class Server
+{
+
+    public static ServerInfo serverInfo;
+
+    static void Main()
+    {
+        serverInfo = new ServerInfo();
+        // проверка создания папки хранения, если ее нет, то создаем
+        if (!Directory.Exists(serverInfo.StoragePath))
+            Directory.CreateDirectory(serverInfo.StoragePath);
+
+        TcpListener listener = new TcpListener(IPAddress.Any, serverInfo.Port); // создаем сокет, принимающий входящие соединения от клиентов
+        listener.Start(); // запуск сервера
+        Console.WriteLine("Сервер запущен на порту: " + serverInfo.Port);
+
+        while (true)
+        {
+            try
+            {
+                TcpClient client = listener.AcceptTcpClient(); // ожидание подключения
+                lock (serverInfo.lockObject) { serverInfo.activeClients++; }
+                Console.WriteLine("Клиент подключился. Активных подключений: " + serverInfo.activeClients);
+                Server class1 = new Server();
+
+                Thread thread = new Thread(serverInfo.HandleClient); // новый поток для обработки клиента
+                thread.Start(client);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка при подключении: " + ex.Message);
+            }
+        }
+    }
+}
